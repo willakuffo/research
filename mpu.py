2329d5e7ae7e49eceb6d1d2a938f9e9ee1ae6960
willakuffo <william.hawkins71@gmail.com>
2020-06-18 14:05:33 +0100

    inital linear acc fnc

--- mpu.py
+++ mpu.py
@@ -2,15 +2,27 @@ import mpu6050
 m = mpu6050.mpu6050(0x68)
 import math
 
-x,y,z = 0,0,0
+ax,ay,az = 0,0,0
+gravity = 9.81
+
+def linear_acc():
+	'''calc linear acc in 2d xy plane'''
+	#expected acc for RNA
+	axy= math.sqrt(gravity**2 - az**2)
+	return axy
+	
+	
+	
+	
+	
 while True:
 	accel_data =m.get_accel_data()
-	x,y,z = accel_data['x'],accel_data['y'],accel_data['z']
+	ax,ay,az = accel_data['x'],accel_data['y'],accel_data['z']
 	
 	#RNA
-	xynet = math.sqrt(x**2+y**2)
-	rna = math.sqrt(xynet**2+z**2) 
+	xynet = math.sqrt(ax**2+ay**2)
+	rna = math.sqrt(xynet**2+az**2) 
 	
 	
-	print(x,y,z,rna)
-	
+	#print(ax,ay,az,rna)
+	print(linear_acc())
